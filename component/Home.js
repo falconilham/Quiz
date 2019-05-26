b8449cfe6cf10e2384d20c5a60076e2334c8bc7f
Ilham <ilhama36@.com>
2019-05-26 22:38:02 +0700

    Fuckin Almost Perfect

--- component/Home.js
+++ component/Home.js
@@ -2,8 +2,6 @@ import React, {Component} from 'react';
 import {StyleSheet, View, Text, TextInput, Button, Picker} from 'react-native';
 import Soal from './Soal.json';
 
-const soal_test = Soal
-const soal = 0
 export default class Home extends Component {
   constructor() {
     super();
@@ -11,20 +9,21 @@ export default class Home extends Component {
       name: '',
       condition: 1,
       soal: Soal,
-      soal_i: 0,
       nomor_soal: 1,
       soal_dipakai: Soal[Math.floor(Math.random()*Soal.length)],
       total_soal_dikerjakan: 0,
+      soal_test:[] ,
       nilai: 0,
       jawaban: "",
     }
+    this.getData()
   }
 
-  //gantisoal
-  changeSoal = () =>{
+  getData(){
   }
+  
   componentDidMount(){
-    //this.getData();
+    //this.getData();  
   }
 
   //Cek User
@@ -42,56 +41,64 @@ export default class Home extends Component {
   }
   //cek nilai
   cekNilai = (e, i,item) => {
-    if(this.state.total_soal_dikerjakan === 20){
-      alert("total nilai anda "+this.state.nilai)
-    }else{
-      if(item === this.state.soal_dipakai.jawaban_benar){
-        this.setState({
-          nilai: this.state.nilai + 1 * 10 / 2,
-          nomor_soal : this.state.nomor_soal + 1,
-        })
-      }else{
-        this.setState({
-          nilai : this.state.nilai + 0 * 10 / 2,
-          nomor_soal : this.state.nomor_soal + 1
-        })
-      }
+    if(item === this.state.soal_dipakai.jawaban_benar){
       this.setState({
-        soal_dipakai: this.state.soal[Math.floor(Math.random()*Soal.length)],
-        total_soal_dikerjakan : this.state.total_soal_dikerjakan + 1 
+        nilai: this.state.nilai + 1 * 10 / 2,
+        nomor_soal : this.state.nomor_soal + 1,
+      })
+    }else{
+      this.setState({
+        nilai : this.state.nilai + 0 * 10 / 2,
+        nomor_soal : this.state.nomor_soal + 1
+      })
+    }
+    this.setState({
+      soal_dipakai: this.state.soal[Math.floor(Math.random()*Soal.length)],
+      total_soal_dikerjakan : this.state.total_soal_dikerjakan + 1 
+    })
+    if(this.state.total_soal_dikerjakan === 19){
+      this.setState({
+        condition : 3
       })
     }
   }
 
   render() {
-    {if (this.state.condition === 1){ 
+    {if(this.state.condition === 1){ 
       return (
       <View style={styles.container}>
-        <Text style={styles.font_login}>Masukan Nama{this.state.soal.length}</Text>
+        <Text style={styles.font_login}>Masukan Nama{this.state.soal_test[0]}</Text>
         <TextInput style={styles.login_name} onChangeText={(name) => this.setState({name})} placeholder="Input Nama Anda" />
         <Button color="#a3a2f1" title="login" type="clear" onPress={() => this.cekUser()} />
       </View>
-      )}else {
+      )}else if(this.state.condition == 2){ 
         return (
-      <View>
-        <Text>Silakan Mengerjakan {this.state.name}</Text>
-        <Button title="Ganti Nama" width="10" onPress={() => this.setState({condition : 1})} />
+      <View style={{flex: 1, flexDirection: "column"}}>
+        <Text style={{fontWeight: "bold", justifyContent:"center", alignSelf:"center", color: "blue", marginBottom: 20}}>Silakan Mengerjakan {this.state.name}</Text>
         <Text>{this.state.nomor_soal}.{this.state.soal_dipakai.Soal}</Text>
           {this.state.soal_dipakai.jawaban.map((item, i ) => {
           return(
-          <View key={i} style={{ justifyContent: 'center', alignItems: "center"}}>
-            <Text>{i.toString(26).toUpperCase()}</Text>
+          <View key={i} style={{ backgroundColor: "white"}}>
+            <Text>{String.fromCharCode(i + 1 + 64)}</Text>
             <Button onPress={() => this.cekNilai(this, i, item)} title={item} />
           </View>
           )
           })}
-          <View style={{fontSize: 50, justifyContent: 'center', alignItems: "center"}}>
-            <Text>Nilai Anda</Text>
-            <Text>{this.state.nilai}</Text>
-            <Text>{this.state.total_soal_dikerjakan}</Text>
-          </View>
       </View>
-      )}
+      )}else{
+        return (
+        <View style={styles.container}>
+          <Text>nilai Anda {this.state.nilai}</Text>
+          { this.state.nilai < 50 ? (
+            <Text>Anda Bodoh Sekali</Text>
+          ):(
+            <Text>OK</Text>
+          )
+          }
+          <Button title="Reset" onPress={() => this.setState({ condition : 1, nomor_soal: 1, name : "", nilai: 0, total_soal_dikerjakan: 0})} />
+        </View>
+        )
+      }
     }
   }
 }
