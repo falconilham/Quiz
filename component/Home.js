1c6507b3572b49f5629bdb9c1d6555f5e2d38c07
unknown <ilhama36@gmail.com>
2019-05-30 17:39:46 +0700

    UI Improvement

--- component/Home.js
+++ component/Home.js
@@ -72,19 +72,23 @@ export default class Home extends Component {
       )}else if(this.state.condition == 2){ 
         return (
       <View style={styles.container}>
-        <View style={styles.form_login}>
-          <Text style={{fontWeight: "bold", justifyContent:"center", alignSelf:"center", color: "blue", marginBottom: 20}}>Silakan Mengerjakan {this.state.name}</Text>
+      <Text style={{fontWeight: "bold", justifyContent:"flex-start",top: 0, position: "absolute",marginHorizontal: 10 , alignSelf:"flex-start", color: "white"}}>Silakan Mengerjakan {this.state.name}</Text>
+        <View style={styles.form_soal}>
           <Text>{this.state.nomor_soal}.{this.state.soal_dipakai[this.state.current].Soal}</Text>
+        </View>
+        <View style={styles.form_jawaban}>
           <View style={{flex : 1, flexDirection: 'row', flexWrap: "wrap", alignItems: 'flex-start', justifyContent: "space-around"}}>
             {this.state.soal_dipakai[this.state.current].jawaban.map((item, i ) => {
             return(
-            <View key={i} style={{width: "35%", margin: 10}}>
-              <Text>{String.fromCharCode(i + 1 + 64)}</Text>
+            <View key={i} style={{width: "35%", marginVertical: 10, marginTop: 20}}>
               <Button onPress={() => this.cekNilai(this, i, item)} title={item} />
             </View>
             )
             })}
           </View>
+        </View>
+        <View style={{bottom: 100, position: "absolute", color: "white"}}>
+            <Text>Good Luck</Text>
         </View>
       </View>
       )}else{
@@ -142,7 +146,6 @@ const styles = StyleSheet.create({
     width: "80%",
     margin: 10,
     backgroundColor:"#eae9e9",
-    fontSize: 10,
     marginVertical: 25,
   },
   button_login:{
@@ -165,6 +168,24 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    
+  },
+  form_jawaban:{
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:"white",
+    height: "35%",
+    width: "80%",
+    borderRadius: 10,
+  },
+  form_soal:{
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:"white",
+    height: "10%",
+    width: "80%",
+    marginBottom: 30,
+    borderRadius: 10
   }
 });
